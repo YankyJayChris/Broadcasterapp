c862955eb7c9dfa5f62b3b1cf32cb0d7586bd97b
IGIHOZO Jean Christian <igihozo35@gmail.com>
2019-11-25 01:01:09 +0200

    feat(POST /red-flags): Create a red-flag record feature
    - create post request endpoint
    - created create red-flag controller
    
    [Finishes #169941335]

--- server/src/routes/RedFlags.js
+++ server/src/routes/RedFlags.js
@@ -5,10 +5,13 @@ import RedFlags from '../controllers/RedFlags';
 
 // redflag middleware
 import jwtToken from '../helpers/jwtToken';
+import redFlagValidator from '../validation/RedFlag';
+import { uploadFiles } from '../helpers/upload';
 
 const router = express.Router();
 
-router.post('/', (req, res) => res.status(200).json({ msg: 'it\'s works' }));
+router.post('/test', (req, res) => res.status(200).json({ msg: 'it\'s works' }));
 router.get('/', jwtToken.verifyToken, RedFlags.getAll);
+router.post('/', jwtToken.verifyToken, uploadFiles, redFlagValidator('redFlagPost', 'body'), RedFlags.create);
 
 export default router;
