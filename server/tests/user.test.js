ff9d8aef39f06143bee0a109fa925caf8183fa85
IGIHOZO Jean Christian <igihozo35@gmail.com>
2019-11-29 00:44:24 +0200

    chore(red-flag test): create test of red-flag endpoint
    - create test file
    - create success test
    - create error test
    
     [Finishes #170018690]

---
+++ server/tests/user.test.js
@@ -0,0 +1,70 @@
+// import the dev-dependencies
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../server';
+
+process.env.NODE_ENV = 'test';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+describe('GET /api/v1/auth', () => {
+  const user = {
+    firstname: 'dizzo',
+    lastname: 'did did',
+    username: 'itsmejaja',
+    email: 'dizzo@gmail.com',
+    phoneNumber: '0721314151',
+    password: 'Password12',
+    re_password: 'Password12',
+  };
+  let authData;
+  before((done) => {
+    chai.request(server)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        authData = res.body.data;
+        done();
+      });
+  });
+
+  it('User should be able to get all users', (done) => {
+    chai.request(server)
+      .get('/api/v1/auth/')
+      .set('x-access-token', authData.token)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+  it('User should not be able to get all users if there is no token', (done) => {
+    chai.request(server)
+      .get('/api/v1/auth/')
+      .end((err, res) => {
+        expect(res).to.have.status(403);
+        expect(res.body).to.have.a.property('error');
+        done();
+      });
+  });
+  it('User should be able to get single user', (done) => {
+    chai.request(server)
+      .get(`/api/v1/auth/${authData.user.id}`)
+      .set('x-access-token', authData.token)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+  it('User should be able to delete single user', (done) => {
+    chai.request(server)
+      .delete(`/api/v1/auth/${authData.user.id}`)
+      .set('x-access-token', authData.token)
+      .end((err, res) => {
+        expect(res).to.have.status(204);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+});
